a0657cf890af7c44debe66d82261d9c116020557
Faisal <faisalakbar661@gmail.com>
2025-01-21 10:22:02 +0700

    test script9.1

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -12,7 +12,7 @@ git config --global http.postBuffer 157286400
 git clone --branch gh-pages https://Faisal-666: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@github.com/Faisal-666/simple-python-pyinstaller-app.git gh-pages-temp
 
 cd gh-pages-temp
-cp -r /var/jenkins_home/workspace/submission-cicd-pipeline-faisalakbar661/dist/*
+cp -r /var/jenkins_home/workspace/submission-cicd-pipeline-faisalakbar661/dist/* .
 
 git add .
 git commit -m "test deploy ke gh-pages"
