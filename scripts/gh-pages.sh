ade71d3e217ea2bc6f7e37f36e95fa8544fe6920
Faisal <faisalakbar661@gmail.com>
2025-01-21 09:45:54 +0700

    test script8.33

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -x
 
+pwd
+
 ls dist
 
 git config --global user.email "faisalakbar661@gmail.com"
@@ -8,4 +10,4 @@ git config --global user.name "Faisal"
 git config --global http.postBuffer 157286400
 git remote set-url origin https://Faisal-666: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@github.com/Faisal-666/simple-python-pyinstaller-app.git
 
-gh-pages --message 'test deploy' --dist /var/jenkins_home/workspace/submission-cicd-pipeline-faisalakbar661/dist
+npx gh-pages --message 'test deploy' --dist dist
