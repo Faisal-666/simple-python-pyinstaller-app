efbbe8a1321ced390fdf441e934dfbff49e729bb
Faisal <faisalakbar661@gmail.com>
2025-01-21 08:57:59 +0700

    test script6

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -8,4 +8,4 @@ git config --global user.name "Faisal"
 git config --global http.postBuffer 157286400
 git remote set-url origin https://Faisal-666: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@github.com/Faisal-666/simple-python-pyinstaller-app.git
 
-npx gh-pages --message 'test deploy' --dist dist --verbose
+npx gh-pages --message 'test deploy' --dist dist
